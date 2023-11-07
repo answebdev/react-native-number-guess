71a7952b30ee4af29026724006dc0c7cbaba7e2a
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-06 19:32:19 -0800

    add logic to switch screens

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
     const [enteredNumber, setEnteredNumber] = useState('');
 
     function numberInputHandler(enteredText) {
@@ -35,7 +35,7 @@ function StartGameScreen() {
             // Show alert if invalid
         }
 
-        console.log('Valid number!');
+        onPickNumber(chosenNumber);
     }
 
     return (
