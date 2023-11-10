5251ad043970aba07d66472b95c1b2a349bf938e
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-09 19:31:41 -0800

    update styles of buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -70,13 +70,16 @@ function GameScreen({ userNumber, onGameOver }) {
             <Title>Opponent's Guess</Title>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card>
-                <InstructionText>Higher or lower?</InstructionText>
-                <View>
-                    <PrimaryButton myOnPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
-                    <PrimaryButton myOnPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton myOnPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton myOnPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                    </View>
                 </View>
             </Card>
-            {/* <View>LOG ROUNDS</View> */}
         </View>
     );
 }
@@ -87,5 +90,14 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 24
+    },
+    instructionText: {
+        marginBottom: 12
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
